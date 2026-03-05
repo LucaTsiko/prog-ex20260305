f180f32abfc6862356b01f55339657b2c0bd6cf0
Luca Tsiklauri <ltsik@myuax.com>
2026-03-05 09:13:36 +0100

    feat (Main): creación un nuevo centro FP y 3 alumnos.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        CentroFP UAX;
 
+        Alumno alumno1;
+        Alumno alumno2;
+        Alumno alumno3;
+
+        // Nuevo centro de FP
+        UAX = new CentroFP(20);
+
+        // Nuevos alumnos
+        alumno1 = new Alumno(1,"Luca", "DAM", 19);
+        alumno2 = new Alumno(2,"Victor Payo", "DAM", 5);
+        alumno3 = new Alumno(3,"Kikovich", "DAM", 18);
     }
 }
