d4ccc1bda673f6a5733525d53cdafaba1a594459
Luca Tsiklauri <ltsik@myuax.com>
2026-03-05 09:26:04 +0100

    feat (Main): Registro a los 3 alumnos y los muestro.

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,16 @@ public class Main {
         alumno1 = new Alumno(1,"Luca", "DAM", 19);
         alumno2 = new Alumno(2,"Victor Payo", "DAM", 5);
         alumno3 = new Alumno(3,"Kikovich", "DAM", 18);
+
+        // registro alumnos
+        UAX.registrarAlumno(alumno1);
+        UAX.registrarAlumno(alumno2);
+        UAX.registrarAlumno(alumno3);
+
+        // Muestro a los alumnos
+        System.out.println("Alumnos registrados: ");
+        System.out.println(UAX.mostrarAlumnos());
     }
+
+
 }
